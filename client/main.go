ff9d7256d0955ac62dbf2bd6f0fd9b5f76a271b6
Christopher Waldon <christopher.waldon.dev@gmail.com>
2018-03-03 22:35:33 -0500

    Switch to gocui for greater UI flexibility

--- client/main.go
+++ client/main.go
@@ -2,74 +2,65 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
 
-	ui "github.com/gizak/termui"
+	"github.com/jroimartin/gocui"
 	messages "github.com/whereswaldon/arbor/messages"
 )
 
-const inputHeight = 5
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
 
 func main() {
 	if len(os.Args) < 2 {
 		log.Println("Usage: " + os.Args[0] + " <host:port>")
 		return
 	}
-	err := ui.Init()
+	ui, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
 		log.Println("Unable to launch ui", err)
 		return
 	}
 	defer ui.Close()
 
-	requests := make(chan string)
-	msgList := NewList(messages.NewStore())
-	msgList.Overflow = "wrap"
-	msgList.Items = []string{}
-	msgList.BorderLabel = "Messages"
-	msgList.Width = ui.TermWidth()
-	msgList.Height = ui.TermHeight() - inputHeight
-	msgList.Align()
-
-	ui.Render(msgList)
-
-	ui.Handle("/sys/kbd/q", func(ui.Event) {
-		ui.StopLoop()
-	})
-
-	ui.Handle("/arbor/new_message", func(e ui.Event) {
-		msgList.UpdateMessage(e.Data.(string))
-		msgList.Align()
-		ui.Render(msgList)
-	})
-	ui.Handle("/arbor/request_message", func(e ui.Event) {
-		id := e.Data.(string)
-		requests <- id
-	})
+	queries := make(chan string)
+	layoutManager := NewList(messages.NewStore(), queries)
+	ui.Highlight = true
+	ui.Cursor = true
+	ui.SelFgColor = gocui.ColorGreen
+	ui.SetManager(layoutManager)
+	ui.SetCurrentView("message-history")
 
 	conn, err := net.Dial("tcp", os.Args[1])
 	if err != nil {
 		log.Println("Unable to connect", err)
 		return
 	}
+	go handleConn(conn, layoutManager, ui)
+	go handleRequests(conn, queries)
 
-	go handleConn(conn, msgList)
-	go handleRequests(conn, requests)
+	if err := ui.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
 
-	ui.Loop()
+	if err = ui.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Println("error with ui:", err)
+	}
 }
 
 type MessageListView struct {
-	*ui.List
 	*messages.Store
 	LeafID string
+	Query  chan string
 }
 
-func NewList(store *messages.Store) *MessageListView {
-	return &MessageListView{ui.NewList(), store, ""}
+func NewList(store *messages.Store, queryChan chan string) *MessageListView {
+	return &MessageListView{store, "", queryChan}
 }
 
 func (m *MessageListView) UpdateMessage(id string) {
@@ -77,13 +68,16 @@ func (m *MessageListView) UpdateMessage(id string) {
 	if msg.Parent == m.LeafID || m.LeafID == "" {
 		m.LeafID = msg.UUID
 	}
-	m.regenerateItems()
+	m.getItems()
 }
 
-func (m *MessageListView) regenerateItems() {
+func (m *MessageListView) getItems() []string {
 	const length = 10
 	items := make([]string, length)
 	current := m.Store.Get(m.LeafID)
+	if current == nil {
+		return items
+	}
 	parentID := current.Parent
 	for i := length - 1; i >= 0; i-- {
 		if parentID == "" {
@@ -93,18 +87,49 @@ func (m *MessageListView) regenerateItems() {
 		current = m.Store.Get(parentID)
 		if current == nil {
 			//request the message corresponding to parentID
-			ui.SendCustomEvt("/arbor/request_message", parentID)
+			m.Query <- parentID
 			break
 		}
 		parentID = current.Parent
 	}
-	m.List.Items = items
+	return items
 }
 
-func handleConn(conn io.ReadWriteCloser, mlv *MessageListView) {
+func (m *MessageListView) Layout(ui *gocui.Gui) error {
+	maxX, maxY := ui.Size()
+	if v, err := ui.SetView("message-history", 0, 0, maxX-1, maxY-5); err != nil {
+		if err != gocui.ErrUnknownView {
+			log.Println(err)
+			return err
+		}
+		v.Title = "History"
+		v.Wrap = true
+		v.Autoscroll = true
+	}
+	history, _ := ui.View("message-history")
+	items := m.getItems()
+	for _, item := range items {
+		fmt.Fprintln(history, item)
+	}
+	log.Println("rendered history")
+	if v, err := ui.SetView("message-input", 0, maxY-4, maxX-1, maxY-1); err != nil {
+		if err != gocui.ErrUnknownView {
+			log.Println(err)
+			return err
+		}
+		v.Title = "Compose"
+		v.Editable = true
+		v.Wrap = true
+	}
+	log.Println("rendered copose")
+	return nil
+}
+
+func handleConn(conn io.ReadWriteCloser, mlv *MessageListView, ui *gocui.Gui) {
 	data := make([]byte, 1024)
 	for {
 		n, err := conn.Read(data)
+		log.Println("read ", n, " bytes")
 		if err != nil {
 			log.Println("unable to read message: ", err)
 			return
@@ -119,7 +144,8 @@ func handleConn(conn io.ReadWriteCloser, mlv *MessageListView) {
 		case messages.NEW_MESSAGE:
 			// add the new message
 			mlv.Add(a.Message)
-			ui.SendCustomEvt("/arbor/new_message", a.Message.UUID)
+			mlv.UpdateMessage(a.Message.UUID)
+			ui.Update(func(*gocui.Gui) error { return nil })
 		default:
 			log.Println("Unknown message type: ", string(data))
 			continue
