14cbd6862b8a02dff38e03ed3761e3c3a0fe3b6f
Chris Waldon <christopher.waldon.dev@gmail.com>
2018-10-20 13:50:05 -0400

    Fix doc typo

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// Package arbor library implementation of the Arbor chat protocol.
+// Package arbor is an implementation of the Arbor chat protocol.
 //
 // This package provides tools for reading and writing Arbor protocol
 // messages from io.Readers and io.Writers, as well as a simple concurrent
