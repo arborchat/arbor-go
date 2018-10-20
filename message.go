de329b4e2058bf3a63ae941e0ef49dd6ff396ea2
Chris Waldon <christopher.waldon.dev@gmail.com>
2018-10-20 13:23:06 -0400

    Document Message struct and functions

--- message.go
+++ message.go
@@ -1,11 +1,13 @@
 package messages
 
 import (
+	"time"
+
 	uuid "github.com/nu7hatch/gouuid"
 	"github.com/pkg/errors"
-	"time"
 )
 
+// Message represents a single chat message sent between users.
 type Message struct {
 	UUID      string
 	Parent    string
@@ -14,6 +16,9 @@ type Message struct {
 	Timestamp int64
 }
 
+// NewMessage constructs a Message with the provided content.
+// It's not necessary to create messages with this function,
+// but it sets the timestamp for you.
 func NewMessage(content string) (*Message, error) {
 	return &Message{
 		Parent:    "",
@@ -23,6 +28,8 @@ func NewMessage(content string) (*Message, error) {
 
 }
 
+// AssignID generates a new UUID and sets it as the ID for the
+// message.
 func (m *Message) AssignID() error {
 	id, err := uuid.NewV4()
 	if err != nil {
@@ -32,6 +39,8 @@ func (m *Message) AssignID() error {
 	return nil
 }
 
+// Reply returns a new message with the given content that has
+// its parent, content, and timestamp already configured.
 func (m *Message) Reply(content string) (*Message, error) {
 	reply, err := NewMessage(content)
 	if err != nil {
