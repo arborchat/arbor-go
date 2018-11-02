e9bf88af99368d56e46fa6ed68d5529574fc9caf
Chris Waldon <christopher.waldon.dev@gmail.com>
2018-11-01 23:00:41 -0400

    Ensure that MakeMessageWriter terminates on error

--- io.go
+++ io.go
@@ -9,7 +9,9 @@ import (
 // MakeMessageWriter wraps the io.Writer and returns a channel of
 // ProtocolMessage pointers. Any ProtocolMessage sent over that channel will be
 // written onto the io.Writer as JSON. This function handles all
-// marshalling.
+// marshalling. If a message fails to marshal for any reason, or if a write error
+// occurs, the returned channel will be closed and no further messages will be
+// written to the io.Writer.
 func MakeMessageWriter(conn io.Writer) chan<- *ProtocolMessage {
 	input := make(chan *ProtocolMessage)
 	go func() {
@@ -23,6 +25,7 @@ func MakeMessageWriter(conn io.Writer) chan<- *ProtocolMessage {
 					return
 				}
 				log.Println("Error encoding message", err)
+				return
 			}
 		}
 	}()
@@ -32,7 +35,7 @@ func MakeMessageWriter(conn io.Writer) chan<- *ProtocolMessage {
 // MakeMessageReader wraps the io.ReadCloser and returns a channel of
 // ProtocolMessage pointers. Any JSON received over the io.ReadCloser will
 // be unmarshalled into an ProtocolMessage struct and sent over the returned
-// channel. If invalid JSON is received, the ReadCloser will close the TCP connection
+// channel. If invalid JSON is received, the ReadCloser will close the io.ReadCloser
 // and the returned channel.
 func MakeMessageReader(conn io.ReadCloser) <-chan *ProtocolMessage {
 	output := make(chan *ProtocolMessage)
